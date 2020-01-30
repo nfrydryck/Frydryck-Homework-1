571d8d74bb8b672ec885e4ccbb75b523fbfbc5bb
Natalie Frydryck <n.frydryck@setonhill.edu>
2020-01-29 19:06:32 -0500

    Initial Commit

---
+++ Frydryck Homework 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Frydryck Homework 1
+//
+//  Created by Natalie Frydryck on 1/29/20.
+//  Copyright © 2020 Natalie Frydryck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
